18a0fb3ef5a67f8091de048378aa75d206c359a3
Ben Towler <b.towler.54@gmail.com>
2020-06-23 18:58:24 -0400

    chore: convert to TS

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import * as ShallowRenderer from 'react-test-renderer/shallow';
 import App from './App';
 
 describe('App', () => {
   describe('App Matches Snapshot', () => {
     test('snapshot matches', () => {
-      const renderer = new ShallowRenderer();
+      const renderer = ShallowRenderer.createRenderer();
       const result = renderer.render(<App />);
       expect(result).toMatchSnapshot();
     });
